b5018634b49ba5a167e13d0a01e06bb3742f8ce6
vadzimavich <vadzimavich@gmail.com>
2025-07-28 01:52:01 +0300

    feat: implement basic routing with pages (Home, About, 404) and layout

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,14 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App.tsx';
+
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import Layout from './components/Layout/Layout.tsx';
+import HomePage from './pages/HomePage.tsx';
+import AboutPage from './pages/AboutPage.tsx';
+import NotFoundPage from './pages/NotFoundPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </StrictMode>
 );
---
+++ class-components/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to the Pokedex!</h1>
+      <p>Content will be loaded here soon...</p>
+    </div>
+  );
+};
+
+export default HomePage;
